cff8975fc2b76ede12dfbcaf780419f127ebc81a
Burhan Aras <burhan.aras@paydustry.com>
2021-06-15 23:13:51 +0300

    Bismillah.Initial version.

---
+++ Modern-MVVM-SwiftUI/Modern_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modern_MVVM_SwiftUIApp.swift
+//  Modern-MVVM-SwiftUI
+//
+//  Created by Burhan Aras on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Modern_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
